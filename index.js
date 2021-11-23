717c816d0de59d18a766d83d0bc15a6fee4dc92c
Johnny Wu <johnnyw131@gmail.com>
2021-11-22 19:41:57 -0500

    commit tree 8109b59bc62cc1df3ab6e4f84f3f256ac6eadd4c

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(X){
+if (X <= 400){
+  return 'This one is on me!';
+} else if (X > 2000 && X < 2500){
+return "I will gladly take your thirty bucks.";
+} else if (X > 2500){
+  return "No can do.";
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+function ternaryCheckCity(City){
+ // Write your code here!
+return City === 'NYC'? 'Ok, sounds good.': 'No go.'
+ }
+
+
+function switchOnCharmFromTip(Tip){
+   // Write your code here!
+   switch (Tip){
+     case 'generous':
+      return 'Thank you so much.'
+     case 'not as generous':
+      return 'Thank you.'
+    default:
+      return "Bye."
+   }
+   }
